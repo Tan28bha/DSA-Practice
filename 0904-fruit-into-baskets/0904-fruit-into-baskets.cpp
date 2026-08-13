cb7bf0016e981ea90ca61d314b08103e1996dbf6
Tanmay Bhadauria <145855521+Tan28bha@users.noreply.github.com>
2026-08-13 17:41:12 +0530

    Time: 47 ms (43.33%), Space: 84.9 MB (34.64%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -9,22 +9,17 @@ public:
 
         for (int right = 0; right < fruits.size(); right++) {
 
-            // Add current fruit
             freq[fruits[right]]++;
 
-            // More than 2 fruit types
             while (freq.size() > 2) {
-
                 freq[fruits[left]]--;
 
-                if (freq[fruits[left]] == 0) {
+                if (freq[fruits[left]] == 0)
                     freq.erase(fruits[left]);
-                }
 
                 left++;
             }
 
-            // Valid window
             ans = max(ans, right - left + 1);
         }
 
