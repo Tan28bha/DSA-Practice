e04ee07b784984de70628b1c5fc2dc97e71dc732
Tanmay Bhadauria <145855521+Tan28bha@users.noreply.github.com>
2026-08-18 10:03:52 +0530

    Time: 0 ms (100%), Space: 35.7 MB (79.48%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+
+        int totalSum = 0;
+
+        // Calculate total sum
+        for (int x : nums) {
+            totalSum += x;
+        }
+
+        int leftSum = 0;
+
+        // Find leftmost pivot
+        for (int i = 0; i < nums.size(); i++) {
+
+            int rightSum = totalSum - leftSum - nums[i];
+
+            if (leftSum == rightSum) {
+                return i;
+            }
+
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+};
